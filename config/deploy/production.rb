1b2cac8f9edcc3ff0ee2d97406984728cdb8d38b
Robert <robert@rqfh.us.to>
2017-03-13 14:41:56 -0700

    Configure server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,5 @@
-set :user, "albert"
+set :user, "robert"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
-server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
+server "rqfh.us.to", :roles => %w(web app db), :primary => true, :user => "robert"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
